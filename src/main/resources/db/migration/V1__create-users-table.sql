4af80f36c35772a7f8a59781658dd60f107d2d08
marcospet <marcos.sousa@petlove.com.br>
2023-07-18 14:20:32 -0300

    feat: ajustado migration

--- src/main/resources/db/migration/V1__create-users-table.sql
+++ src/main/resources/db/migration/V1__create-users-table.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` VARCHAR(100) NULL,
   `email` VARCHAR(50) NULL,
   `password` VARCHAR(255) NULL,
+   `status` TINYINT NULL DEFAULT 0,
   `created_at` DATETIME NULL,
   `update_at` DATETIME NULL,
   `deleted_at` DATETIME NULL,
