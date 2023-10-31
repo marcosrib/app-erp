b25d006408a5f429677013812c543e2e33f7d68c
marcospet <marcos.sousa@petlove.com.br>
2023-10-31 13:10:13 -0300

    test: Adicionado teste create ability category

--- src/main/java/br/com/somar/app/common/config/beans/UserConfig.java
+++ src/main/java/br/com/somar/app/common/config/beans/UserConfig.java
@@ -1,14 +1,13 @@
 package br.com.somar.app.common.config.beans;
 
 import br.com.somar.app.users.application.core.usecases.users.CreateUserUseCase;
-import br.com.somar.app.users.application.ports.in.users.CreateUserUseCasePort;
-import br.com.somar.app.users.application.ports.out.profiles.FindProfileAdapterPort;
 import br.com.somar.app.users.application.core.usecases.users.FindPaginationUserUseCase;
 import br.com.somar.app.users.application.core.usecases.users.UpdateUserUseCase;
+import br.com.somar.app.users.application.ports.in.users.CreateUserUseCasePort;
 import br.com.somar.app.users.application.ports.in.users.FindPaginationUserUseCasePort;
 import br.com.somar.app.users.application.ports.in.users.UpdateUserUseCasePort;
+import br.com.somar.app.users.application.ports.out.profiles.FindProfileAdapterPort;
 import br.com.somar.app.users.application.ports.out.users.*;
-import br.com.somarr.app.users.application.ports.out.users.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
---
+++ src/test/java/br/com/somar/app/unit/users/application/core/usecases/abilitycategories/CreateAbilityCategoryUseCaseTest.java
@@ -0,0 +1,89 @@
+package br.com.somar.app.unit.users.application.core.usecases.abilitycategories;
+
+import br.com.somar.app.unit.users.builders.domain.AbilityCategoryFakeBuilder;
+import br.com.somar.app.unit.users.builders.fileproperties.AbilityFilePropertiesFakeBuilder;
+import br.com.somar.app.users.adapters.outbound.fileproperties.AbilityFileProperties;
+import br.com.somar.app.users.application.core.domain.AbilityCategory;
+import br.com.somar.app.users.application.core.usecases.abilitycategories.CreateAbilityCategoryUseCase;
+import br.com.somar.app.users.application.ports.out.abilitycategories.CreateAbilityCategoryAdapterPort;
+import br.com.somar.app.users.application.ports.out.abilitycategories.FindAbilityCategoryAdapterPort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class CreateAbilityCategoryUseCaseTest {
+    @InjectMocks
+    private CreateAbilityCategoryUseCase createAbilityCategoryUseCase;
+    @Mock
+    private FindAbilityCategoryAdapterPort findAbilityCategoryAdapterPort;
+    @Mock
+    private CreateAbilityCategoryAdapterPort createAbilityCategoryAdapterPort;
+
+    private List<AbilityFileProperties> abilityFilePropertiesFakeBuilder;
+
+    private Set<AbilityCategory> abilityCategories;
+    private final String ABILITY_CATEGORY_CODE = "CODE";
+
+    @BeforeEach
+    void setup() {
+        abilityFilePropertiesFakeBuilder = new AbilityFilePropertiesFakeBuilder().getFake(1);
+        abilityCategories = new AbilityCategoryFakeBuilder().getFake(1);
+    }
+
+    @DisplayName("should successfully create ability category existing")
+    @Test
+    void createAbilityExisting() {
+
+        when(findAbilityCategoryAdapterPort.findCodeByIn(anyList())).thenReturn(abilityCategories);
+        when(createAbilityCategoryAdapterPort.createAll(anySet())).thenReturn(abilityCategories);
+
+        var abilityCategoriesRes = createAbilityCategoryUseCase.findOrCreate(abilityFilePropertiesFakeBuilder);
+
+        verify(createAbilityCategoryAdapterPort, times(1)).createAll(anySet());
+        assertTrue(abilityCategoriesRes.size() > 0);
+    }
+
+    @DisplayName("should successfully create ability category is not existing")
+    @Test
+    void createAbilityIsNotExisting() {
+
+        when(findAbilityCategoryAdapterPort.findCodeByIn(anyList())).thenReturn(Collections.emptySet());
+        when(createAbilityCategoryAdapterPort.createAll(anySet())).thenReturn(abilityCategories);
+
+        var abilityCategoriesRes = createAbilityCategoryUseCase.findOrCreate(abilityFilePropertiesFakeBuilder);
+
+        verify(createAbilityCategoryAdapterPort, times(1)).createAll(anySet());
+        assertTrue(abilityCategoriesRes.size() > 0);
+    }
+
+    @DisplayName("should only return ability category if it already exists registered")
+    @Test
+    void returnAbilityCategoryIfItAlreadyExistsRegistered() {
+        var abilityFilePropertiesFakeBuilder = new AbilityFilePropertiesFakeBuilder().getFake();
+        abilityFilePropertiesFakeBuilder.setCode(ABILITY_CATEGORY_CODE);
+        var abilityCategories = new AbilityCategoryFakeBuilder().getFake();
+        abilityCategories.setCode(ABILITY_CATEGORY_CODE);
+
+        when(findAbilityCategoryAdapterPort.findCodeByIn(anyList())).thenReturn(Set.of(abilityCategories));
+        when(createAbilityCategoryAdapterPort.createAll(anySet())).thenReturn(Set.of(abilityCategories));
+
+        var abilityCategoriesRes = createAbilityCategoryUseCase.findOrCreate(List.of(abilityFilePropertiesFakeBuilder));
+
+        verify(createAbilityCategoryAdapterPort, times(0)).createAll(anySet());
+        assertTrue(abilityCategoriesRes.size() > 0);
+    }
+}
